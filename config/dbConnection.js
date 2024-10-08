c81cb6b9ec2f98073b5534fea4bcca5547ca48aa
MOHAMMED-ANEES-M <mhdaneesm3@gmail.com>
2024-10-08 20:20:43 +0530

    dbConnection file  modified

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
     try{
         const connect = await mongoose.connect('mongodb+srv://nestresidency:nestcltcybozom24@nestcluster.6q9gd.mongodb.net/Nest')
         console.log("Database Connected", connect.connection.host, connect.connection.name);
-        console.log("Database String", process.env.CONNECTION_STRING);
     }catch(err){
         console.log(err);
         process.exit(1);
