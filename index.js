f41312b41898bfda99d4dc276b8cd331a29661c7
MOHAMMED-ANEES-M <mhdaneesm3@gmail.com>
2024-10-08 19:56:26 +0530

    dbConnection string modified

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const connectDB = require('./config/dbConnection');
-// const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 var cors = require('cors');
 const dotenv = require('dotenv').config()
