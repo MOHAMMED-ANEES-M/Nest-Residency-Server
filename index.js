8efe66da42528fd2c9f80192c2e775a31bafebc7
MOHAMMED-ANEES-M <mhdaneesm3@gmail.com>
2024-10-10 07:56:03 +0530

    modified cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app=express()
 const port = process.env.PORT || 5001
 
 const corsOptions = {
-    origin: ['https://nestresidencycalicut.in', 'https://www.nestresidencycalicut.in'],
+    origin: ['http://nestresidencycalicut.in', 'http://www.nestresidencycalicut.in'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 };
